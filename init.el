e62c190579037a43ce377b835206c8d15b9f2dbd
Alexander Weber <weberam2@gmail.com>
2026-06-01 15:54:54 -0700

    added whichkey to start automatically, cleaned things up

--- init.el
+++ init.el
@@ -11,6 +11,7 @@
  ;; If there is more than one, they won't work right.
  )
 
+;; PACKAGE
 (require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
                        ("nongnu" . "https://elpa.nongnu.org/nongnu/")
@@ -23,16 +24,22 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+;; VERTICO
+;; provides a minimal and fast vertical completion UI for Emacs, replacing the default completion display with a more streamlined and user-friendly experience.
 (use-package vertico
   :ensure t
   :init (vertico-mode))
 
+;; ORDERLESS
+;;  enables flexible, space-separated, out-of-order matching of completion candidates. It allows you to type parts of a candidate in any order
 (use-package orderless
 :ensure t
 :custom
 (completion-styles '(orderless basic))
 (completion-category-overrides '((file (styles basic partial-completion)))))
 
+;; CONSULT
+;; provides a collection of interactive Emacs commands that leverage the completion UI (like Vertico) to enhance common tasks, such as searching files, buffers, commands, and other items with live preview and more powerful filtering
 (use-package consult
   :ensure t
   :bind (
@@ -41,33 +48,28 @@
          ("M-s j" . consult-outline)
          ))
 
+;; VTERM
 (use-package vterm
   :ensure t)
 (global-set-key (kbd "C-c v") 'vterm)
 
+;; OPTIONS
 ;; Disable startup message
 inhibit-startup-message t
-
 ;; Clear the initial scratch message
 initial-scratch-message nil
-
-;; Disable backup files
-;; make-backup-files nil
-
 ;; Set the frame title format with a palm tree emoji
 frame-title-format (list "🌴 %b")
-
-;; Set Dired listing switches for more minimalist output
-;; dired-listing-switches "-laghov --group-directories-first")
-
 ;; Disable the menu bar
 (menu-bar-mode -1)
-
+;; line numbers
 (setq display-line-numbers-type 'relative)
 (global-display-line-numbers-mode +1)
+;; whichkey
+(which-key-mode)
 
+;; GPTEL
 (setq package-install-upgrade-built-in t)
-
 (use-package gptel
   :ensure t
   :config
@@ -76,10 +78,9 @@ frame-title-format (list "🌴 %b")
         (gptel-make-gemini "Gemini"
           :key #'gptel-api-key-from-auth-source
           :stream t))
-
   ;; Set default model
   (setq gptel-model 'gemini-2.5-flash))
-
 (use-package markdown-mode
   :ensure t)
 (global-set-key (kbd "C-c g") 'gptel)
+
